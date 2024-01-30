50662c5a7ecc57acf6074be24cc34a08627b115a
Ubuntu <ubuntu@ip-172-31-44-45.ap-south-1.compute.internal>
2024-01-30 06:23:43 +0000

    register page and design improvements

--- wp-content/themes/outbuilt-child/functions.php
+++ wp-content/themes/outbuilt-child/functions.php
@@ -21,46 +21,18 @@
 	   woocommerce_login_form( array( 'redirect' => wc_get_page_permalink( 'myaccount' ) ) );
 	   return ob_get_clean();
 	}	
-	
-	/**
-	 * @snippet       WooCommerce User Registration Shortcode 
-	 */
-	   
-	add_shortcode( 'wc_reg_form_bbloomer', 'bbloomer_separate_registration_form' );
-		 
-	function bbloomer_separate_registration_form() {
-	   if ( is_user_logged_in() ) return '<p>You are already registered</p>';
-	   ob_start();
-	   do_action( 'woocommerce_before_customer_login_form' );
-	   $html = wc_get_template_html( 'myaccount/form-login.php' );
-	   $dom = new DOMDocument();
-	   $dom->encoding = 'utf-8';
-	   $dom->loadHTML( utf8_decode( $html ) );
-	   $xpath = new DOMXPath( $dom );
-	   $form = $xpath->query( '//form[contains(@class,"register")]' );
-	   $form = $form->item( 0 );
-	   echo $dom->saveXML( $form );
-	   return ob_get_clean();
+	 
+
+	// Add a register link below the WooCommerce login form 
+	function add_register_link_below_login_form() {
+		?>
+	<p class="woocommerce-LostPassword lost_password">
+		Don't have an account? <a href="<?php echo esc_url(site_url('/register/')); ?>">Register here</a>.
+	</p>
+	<?php
 	}
 
-	/**
-	 * @snippet       Redirect Login/Registration to My Account
-	 * @how-to        Get CustomizeWoo.com FREE
-	 * @author        Rodolfo Melogli
-	 * @compatible    WooCommerce 7
-	 * @community     https://businessbloomer.com/club/
-	 */
-	
-	add_action( 'template_redirect', 'bbloomer_redirect_login_registration_if_logged_in' );
-	
-	function bbloomer_redirect_login_registration_if_logged_in() {
-		if ( is_page() && is_user_logged_in() && ( has_shortcode( get_the_content(), 'wc_login_form_bbloomer' ) || has_shortcode( get_the_content(), 'wc_reg_form_bbloomer' ) ) ) {
-			wp_safe_redirect( wc_get_page_permalink( 'myaccount' ) );
-			exit;
-		}
-	}
+	add_action('woocommerce_login_form', 'add_register_link_below_login_form');
+
 
 ?>
- 
- 
- 
